d1df8cee05136f43abb561e7d9ddafa2e2de3321
PHANI DEEP CHALLA <PXCHALL2@CORP.AD.SENTARA.COM>
2024-12-12 14:00:41 +0530

    update  commit

--- food/views.py
+++ food/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Item
+from .forms import ItemForm
 #from django.template import loader
 
 # Create your views here.
@@ -20,3 +21,36 @@ def detail(request, item_id):
     context = {'item':item}
     return render(request,'food/detail.html' ,context)
     #return HttpResponse("This is item no/id: %s" % item_id)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-form.html',{'form':form})
+
+def update_item(request,id):
+    item = Item.objects.get(id=id)
+
+    form = ItemForm(request.POST or None,instance= item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request,'food/item-form.html',{'form':form,'item':item} )
+
+
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request,'food/item-delete.html',{'item':item})
+
+
+
